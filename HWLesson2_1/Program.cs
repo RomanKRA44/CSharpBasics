e4647f621384aaa003aadf07989281f913a0e481
RomanKRA44 <adm-nppl@yandex.ru>
2022-03-08 13:47:16 +0300

    ДЗ Урок 2.1

--- HWLesson2_1/Program.cs
+++ HWLesson2_1/Program.cs
@@ -6,7 +6,11 @@ namespace HWLesson2_1
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Введите минимальную температуру за сутки: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите максимальную температуру за сутки: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine((a + b) / 2);
         }
     }
 }
