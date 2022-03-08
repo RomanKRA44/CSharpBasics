9042434e4ab8046f6c5b522b92ae55035f986c4b
RomanKRA44 <adm-nppl@yandex.ru>
2022-03-08 14:07:54 +0300

    ДЗ Урок 2.3

--- HWLesson2_1/Program.cs
+++ HWLesson2_1/Program.cs
@@ -6,54 +6,11 @@ namespace HWLesson2_1
     {
         static void Main(string[] args)
         {
-            string monthTitle = "";
-            Console.Write("Введите номер месяца: ");
-            int month = int.Parse(Console.ReadLine());
-            switch (month)
-            {
-                case 1:
-                    monthTitle = "Январь";
-                    break;
-                case 2:
-                    monthTitle = "Февраль";
-                    break;
-                case 3:
-                    monthTitle = "Март";
-                    break;
-                case 4:
-                    monthTitle = "Апрель";
-                    break;
-                case 5:
-                    monthTitle = "Май";
-                    break;
-                case 6:
-                    monthTitle = "Июнь";
-                    break;
-                case 7:
-                    monthTitle = "Июль";
-                    break;
-                case 8:
-                    monthTitle = "Август";
-                    break;
-                case 9:
-                    monthTitle = "Сентябрь";
-                    break;
-                case 10:
-                    monthTitle = "Октябрь";
-                    break;
-                case 11:
-                    monthTitle = "Ноябрь";
-                    break;
-                case 12:
-                    monthTitle = "Декабрь";
-                    break;
-                default:
-                    Console.WriteLine("Укажите значение от 1 до 12");
-                    break;
-                    
-            }
-            
-            Console.WriteLine(monthTitle);
+            Console.Write("Введите Число: ");
+            int x = int.Parse(Console.ReadLine());
+            if (x % 2 == 0) Console.WriteLine("Число четное");
+            else
+                Console.WriteLine("Чило не четное");
 
         }
     }
