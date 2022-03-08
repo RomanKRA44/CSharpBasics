826dc7450f7f73c4774f511da8153a35b4af9ef3
RomanKRA44 <adm-nppl@yandex.ru>
2022-03-08 16:59:29 +0300

    ДЗ к уроку 3.3

--- HWLesson3/HWLesson3/Program.cs
+++ HWLesson3/HWLesson3/Program.cs
@@ -6,26 +6,13 @@ namespace HWLesson3
     {
         static void Main(string[] args)
         {
-            string[,] phoneList = {
-            {"Иван", "352-56-95"},
-            {"Сергей", "578-52-63"},
-            {"Роман", "425-54-36"},
-            {"Александр", "734-65-23"},
-            {"Виталий", "234-67-45"}
-
-            };
-
-            Console.Clear();
-            Console.WriteLine("Телефонный справочник");
-
-            for (int i = 0; i < phoneList.GetLength(0); i++)
-            {
-                for (int j = 0; j < phoneList.GetLength(1); j++)
-                {
-                    System.Console.Write($"{phoneList[i, j]} ");
-                }
-                System.Console.WriteLine();
-            }
+            Console.Write("Введите слово:");
+            string reverse = Console.ReadLine();
+            char[] reversWord = reverse.ToCharArray();
+            Array.Reverse(reversWord);
+            reverse = new string(reversWord);
+            Console.WriteLine(reverse);
+            Console.ReadLine();
         }
     }
 }
