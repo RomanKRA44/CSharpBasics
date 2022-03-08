9a9073b9efe3fb04c94dafd4d9c393297156b13d
RomanKRA44 <adm-nppl@yandex.ru>
2022-03-08 14:04:28 +0300

    ДЗ урок 2.2

--- HWLesson2_1/Program.cs
+++ HWLesson2_1/Program.cs
@@ -6,11 +6,55 @@ namespace HWLesson2_1
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите минимальную температуру за сутки: ");
-            int a = int.Parse(Console.ReadLine());
-            Console.Write("Введите максимальную температуру за сутки: ");
-            int b = int.Parse(Console.ReadLine());
-            Console.WriteLine((a + b) / 2);
+            string monthTitle = "";
+            Console.Write("Введите номер месяца: ");
+            int month = int.Parse(Console.ReadLine());
+            switch (month)
+            {
+                case 1:
+                    monthTitle = "Январь";
+                    break;
+                case 2:
+                    monthTitle = "Февраль";
+                    break;
+                case 3:
+                    monthTitle = "Март";
+                    break;
+                case 4:
+                    monthTitle = "Апрель";
+                    break;
+                case 5:
+                    monthTitle = "Май";
+                    break;
+                case 6:
+                    monthTitle = "Июнь";
+                    break;
+                case 7:
+                    monthTitle = "Июль";
+                    break;
+                case 8:
+                    monthTitle = "Август";
+                    break;
+                case 9:
+                    monthTitle = "Сентябрь";
+                    break;
+                case 10:
+                    monthTitle = "Октябрь";
+                    break;
+                case 11:
+                    monthTitle = "Ноябрь";
+                    break;
+                case 12:
+                    monthTitle = "Декабрь";
+                    break;
+                default:
+                    Console.WriteLine("Укажите значение от 1 до 12");
+                    break;
+                    
+            }
+            
+            Console.WriteLine(monthTitle);
+
         }
     }
 }
